d142a0b64bb0e016c8a9a9d8112e810ef7cffb6b
Iris de la Mora <irismo91@gmail.com>
2025-01-03 09:37:52 +0100

    minor changes

--- app/Filament/Resources/PetResource/Pages/CreatePet.php
+++ app/Filament/Resources/PetResource/Pages/CreatePet.php
@@ -5,8 +5,20 @@ namespace App\Filament\Resources\PetResource\Pages;
 use App\Filament\Resources\PetResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreatePet extends CreateRecord
 {
     protected static string $resource = PetResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Verifica si el usuario autenticado es un "Owner"
+        if (Auth::user()->isOwner()) {
+            // Asigna el ID del usuario al campo 'owner_id'
+            $data['owner_id'] = Auth::id();
+        }
+
+        return $data;
+    }
 }
